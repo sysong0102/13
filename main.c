6ed14ad72739098bff293c090c6c1e80a715b20c
sysong0102 <sysong0102@sookmyung.ac.kr>
2023-11-30 15:29:50 +0900

    Q2

--- main.c
+++ main.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 //형식 선언 
-struct student
+struct point
 {
-	int ID;
-	char name[100]; //100글자 안으로 
-	float grade;
+	int x;
+	int y;
 };
 
 int main(int argc, char *argv[]) 
 {
 	//instance 선언
 	 
-	struct student s1 = {123, "JUYEOP", 4.3}; //변수는 내 맘대로 
+	struct point p1, p2;
+	int xdiff, ydiff;
+	double dist;
 	
-	s1.ID = 123456;
-	s1.name[0] = 'c';
-	s1.grade = 0.7;
+	printf("input p1 coordinate (x y) : ");
+	scanf("%d %d", &p1.x, &p1.y);
 	
-	printf("ID : %i\n", s1.ID);
-	printf("name : %s\n", s1.name);
-	printf("grade : %f\n", s1.grade);
-	
-	
-	strcpy(s1.name, "Bongjun");
-	printf("name2 : %s\n",s1.name);
+	printf("input p1 coordinate (x y) : ");
+	scanf("%d %d", &p2.x, &p2.y);
 	
 	
+	//distance
+	xdiff = p2.x - p1.x;
+	ydiff = p2.y - p1.y;
+	dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+	printf("distance : %f\n", dist);
+
+	system("PAUSE");
 	return 0;
 }
